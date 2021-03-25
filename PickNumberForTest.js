5bfa03b1c002f508daf3a6725a6a981217d0ad5b
cogito1016 <cogito1016@naver.com>
2021-03-25 19:43:03 +0900

    feat: 모의고사

---
+++ PickNumberForTest.js
@@ -0,0 +1,49 @@
+let test = [[1,2,3,4,5],[1,3,2,4,2],[8]];
+for(let i=0; i<test.length;i++){
+    console.log(solution(test[i]));
+}
+
+function solution(answers) {
+    var answer = [];
+
+    let students ={1:{point:0, way:[1,2,3,4,5]},
+    2:{point:0, way:[2,1,2,3,2,4,2,5]},
+    3:{point:0, way:[3,3,1,1,2,2,4,4,5,5]}};
+
+    for(let key in students){
+        let way = students[key].way;
+
+        for(let i=0; i<answers.length; i++){
+            let number = answers[i];
+            console.log(`${number}와 ${way[i%way.length]}를 비교합니다`)
+            if(number===way[i%way.length]){
+                students[key].point++;
+            }
+        }
+    }
+
+    console.log(students)
+
+    let max = -1;
+    for(let key in students){
+        let point = students[key].point;
+        let keyParsedInteger = parseInt(key);
+
+        if(max>point){
+            continue;
+        }
+
+        if(max<point){
+            max = point;
+            answer = [keyParsedInteger];
+            continue;
+        }
+
+        if(max===point){
+            answer.push(keyParsedInteger);
+            continue;
+        }
+    }
+
+    return answer;
+}
